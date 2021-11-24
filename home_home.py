29cc5fafb4d9821c3922595e1f40e50e9353beae
umara <ua9404@rit.edu>
2021-11-24 09:26:14 +0400

    second commit

--- home_home.py
+++ home_home.py
@@ -5,3 +5,14 @@ class Item:
         self.__name = name
         self.__code = code
         self.__price = 0
+
+class Home_Avatar:
+    __slots__ = ['__garden_options', '__indoor_options', '__bathroom_options', '__base_service']
+
+    def __init__(self, garden_options, indoor_options, bathroom_options):
+        self.__garden_options = garden_options
+        self.__indoor_options = indoor_options
+        self.__bathroom_options = bathroom_options
+        self.__base_service = 0
+
+        
