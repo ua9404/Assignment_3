630653fbda752813313a3e7cc1be384a41495363
umara <ua9404@rit.edu>
2021-11-24 09:52:21 +0400

    third commit

--- home_home.py
+++ home_home.py
@@ -13,6 +13,8 @@ class Home_Avatar:
         self.__garden_options = garden_options
         self.__indoor_options = indoor_options
         self.__bathroom_options = bathroom_options
-        self.__base_service = 0
+        self.__base_service = 50
 
-        
+garden_option = {'3 pack garden flower (p)': 5.0, 'Hanging light wire (l)': 10.0, 'garden bench (b)': 35.0, 'None and Next (n)': 0.0}
+indoor_option = {'Small table lamp (t)': 5.0, 'City picture frame (f1)': 7.0, '4x5 entry rug (r)': 35.0, 'None and Next (n)': 0.0}
+bathroom_option = {'Weighing scale (w)': 2.0, 'Towel (a)': 3.0, 'Brush holder (y)': 6.0, 'None and Next': 0.0}
