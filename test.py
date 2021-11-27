d1477fd16ff4d31a252e83fac64ced3d91ba13e3
umara <ua9404@rit.edu>
2021-11-27 17:14:14 +0400

    number 10 commit

---
+++ test.py
@@ -0,0 +1,11 @@
+import home_home
+
+# This test will show if the garden options are printed corresponding to there item code
+def test_garden_options():
+    assert "p" == '3 pack garden flower'
+    assert "l" == 'Hanging light wire'
+    assert "b" == 'garden bench'
+    assert "n" == 'None and Next'
+
+test_garden_options()
+
